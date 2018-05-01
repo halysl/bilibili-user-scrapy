9f00d65bbdcff0909bd6d5a3ad063e5c9b0d743e
light <halysl0817@gmail.com>
2018-05-01 11:39:27 +0800

    first post

---
+++ bilibili_user_scrapy/items.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class BilibiliUserScrapyItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    uid = scrapy.Field()
+    mid = scrapy.Field()
+    name = scrapy.Field()
+    sex = scrapy.Field()
+    coins = scrapy.Field()
+    regtime = scrapy.Field()
+    birthday = scrapy.Field()
+    place = scrapy.Field()
+    fans = scrapy.Field()
+    friend = scrapy.Field()
+    attention = scrapy.Field()
+    level = scrapy.Field()
+    exp = scrapy.Field()
+
